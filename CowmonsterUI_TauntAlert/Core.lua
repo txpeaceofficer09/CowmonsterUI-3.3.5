961afb2548d1f2b8560bd33e7f529793a20d2abc
txpeaceofficer09 <james.mccaughey@gmail.com>
2025-11-23 02:12:35 -0600

    Fix TauntAlert.

--- CowmonsterUI_TauntAlert/Core.lua
+++ CowmonsterUI_TauntAlert/Core.lua
@@ -63,23 +63,42 @@ function f:IsTauntSpell(spellID)
 	return f:find(taunts, spellID)
 end
 
-function f:SPELL_CAST_SUCCESS(self, srcName, srcGUID, dstName, dstGUID, spellID, spellName)
+local function isInGroup(name)
+	if UnitName("player") == name or UnitName("playerpet") == name then return true end
+
+	for i=1,40,1 do
+		if UnitName("party"..i) == name or UnitName("raid"..i) == name or UnitName("party"..i.."pet") == name or UnitName("raid"..i.."pet") == name then
+			return true
+		end
+	end
+
+	return false
+end
+
+function f:SPELL_CAST_SUCCESS(srcName, srcGUID, srcFlags, dstName, dstGUID, dstFlags, spellID, spellName)
 	local msg = nil
+	local link = GetSpellLink(spellID)
 
 	if ( bit.band(srcFlags, COMBATLOG_OBJECT_AFFILIATION_RAID) > 0 or bit.band(srcFlags, COMBATLOG_OBJECT_AFFILIATION_PARTY) > 0 ) and not bit.band(srcFlags, COMBATLOG_OBJECT_AFFILIATION_MINE) > 0 then
 		if bit.band(srcFlags, COMBATLOG_OBJECT_TYPE_PET) then
 			local owner = GetPlayerInfoByGUID(srcGUID)
-			msg = ("%s's pet, %s, taunted %s using %s (%s)."):format(owner, srcName, dstName, spellName, spellID)
+			msg = ("%s's pet, %s, taunted %s using %s (%s)."):format(owner, srcName, dstName, link, spellID)
 		else
-			msg = ("%s taunted %s using %s (%s)."):format(srcName, dstName, spellName, spellID)
+			msg = ("%s taunted %s using %s (%s)."):format(srcName, dstName, link, spellID)
 		end
 	elseif bit.band(srcFlags, COMBATLOG_OBJECT_AFFILIATION_MINE) > 0 then
 		if bit.band(srcFlags, COMBATLOG_OBJECT_TYPE_PET) > 0 then
-			msg = ("My pet, %s, taunted %s using %s (%s)."):format(srcName, dstName, spellName, spellID)
+			msg = ("My pet, %s, taunted %s using %s (%s)."):format(srcName, dstName, link, spellID)
 		else
-			msg = ("I taunted %s using %s (%s)."):format(dstName, spellName, spellID)
+			msg = ("I taunted %s using %s (%s)."):format(dstName, link, spellID)
 		end
 	end
+
+	--[[
+	if ( isInGroup(srcName) ) then
+		msg = ("%s => %s => %s"):format(srcName, spellName, dstName)
+	end
+	]]
 
 	if msg ~= nil then self:print(msg) end
 end
@@ -90,7 +109,7 @@ local function OnEvent(self, event, ...)
 
 		if self[subEvent] then
 			if spellID ~= nil and self:IsTauntSpell(spellID) then
-				self[subEvent](self, srcName, srcGUID, dstName, dstGUID, spellID, spellName)
+				self[subEvent](self, srcName, srcGUID, srcFlags, dstName, dstGUID, dstFlags, spellID, spellName)
 			end
 		end
 	end
