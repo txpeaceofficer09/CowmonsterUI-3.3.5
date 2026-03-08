703367169cef577ac732dda07d4b8609a8a8ddd0
jmccaughey <james@netbit.dev>
2026-03-07 19:51:16 -0600

    Add LoggingCombat to bar to show when combat log is being saved to file.

--- CowmonsterUI_InfoBar/LoggingCombat.lua
+++ CowmonsterUI_InfoBar/LoggingCombat.lua
@@ -1,3 +1,17 @@
+function InfoBarLC_OnEvent(self, event, ...)
+	if event == "COMBAT_LOGGING_STARTED" then
+		InfoBarSetText("InfoBarLC", "|cffffffffLogging Combat: |cff00ff00ON")
+	elseif event == "COMBAT_LOGGING_STOPPED" then
+		InfoBarSetText("InfoBarLC", "|cffffffffLogging Combat: |cffff0000OFF")
+	elseif event == "PLAYER_ENTERING_WORLD" then
+		if not LoggingCombat() then
+			InfoBarSetText("InfoBarLC", "|cffffffffLogging Combat: |cffff0000OFF")
+		else
+			InfoBarSetText("InfoBarLC", "|cffffffffLogging Combat: |cff00ff00ON")
+		end
+	end
+end
+
 function InfoBarLC_OnUpdate(self, elapsed)
 	self.timer = (self.timer or 0) + elapsed
 
